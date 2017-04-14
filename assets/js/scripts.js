19ba3a3f4b9ac4848ad264362248e4b78925152e
Erik Petersen <contacterikpetersen@gmail.com>
2017-04-14 12:42:38 -0400

    Added functionality to compare cards, keep correct ones open, incorrect ones get flipped back

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,35 +5,37 @@ $(function() {
   console.log( "Hello, World!" );
 
   var clicks = 0;
-  var firstCardClass = "";
-  var secondCardClass = "";
+  var firstCard = "";
+  var secondCard = "";
 
   $( "main" ).on( "click", ".card", function() {
 
     clicks++;
     console.log( "Card clicked" );
 
-    // if clicked card's icon is hidden, display it
-    // if( $( this ).children().is( ":hidden" ) ) {
-    //   $( this ).children().show();
-    // }
     $( this ).children().toggle();
 
     if( clicks === 1 ) {
-      firstCardClass = $( this ).children().attr( "class" );
-      console.log( "Click on " + firstCardClass );
+      firstCard = $( this );
+      console.log( $(this).index() );
     } // end if
 
     else if( clicks === 2 ) {
-      secondCardClass = $( this ).children().attr( "class" );
-      console.log( "Click on " + secondCardClass );
+
+      secondCard = $( this );
       clicks = 0;
+
+      if( firstCard.children().attr( "class" ) == secondCard.children().attr( "class" ) ) {
+        firstCard.prop( "disabled", true );
+        secondCard.prop( "disabled", true );
+        console.log( "They match" );
+      }
+
+      else {
+        firstCard.children().delay(1500).hide(1);
+        secondCard.children().delay(1500).hide(1);
+      }
     } // end else if
-
-    else {
-
-    } // end else
-
 
   }); // end click on .card
 
